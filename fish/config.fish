5762f08ffed18345393de5c9a18bf4738863997a
Boris Vassilev <boris.n.vassilev@gmail.com>
2026-09-07 23:52:42 +0300

    fixed fish and hyprpaper

--- fish/config.fish
+++ fish/config.fish
@@ -6,10 +6,10 @@ set fish_greeting
 
 ## Useful aliases
 # Replace ls with exa
-alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
-alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
-alias ll='exa -l --color=always --group-directories-first --icons'  # long format
-alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
+alias ls='exa -al --color=always --group-directories-first --icons always' # preferred listing
+alias la='exa -a --color=always --group-directories-first --icons always'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first --icons always'  # long format
+alias lt='exa -aT --color=always --group-directories-first --icons always' # tree listing
 alias l.="exa -a | egrep '^\.'"                                     # show only dotfiles
 alias ip="ip -color"
 
